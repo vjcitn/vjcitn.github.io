7937c7cfc422e2262ea44318843c5389a08e157b
vjcitn <stvjc@channing.harvard.edu>
2023-02-06 07:05:19 -0500

    new draft with an article

---
+++ R/refs.R
@@ -0,0 +1,13 @@
+#' Personal web site reference material
+#' @note Experimental
+webref = function() {NULL}
+
+#' ontoProc: convenient representations of ontologies for genomic data science
+#' @examples
+#' if (requireNamespace("ontoProc")) {
+#'  cl = ontoProc::getOnto('cellOnto')
+#'  cl3k = c("CL:0000492", "CL:0001054", "CL:0000236", "CL:0000625",
+#'     "CL:0000576", "CL:0000623", "CL:0000451", "CL:0000556")
+#'  ontoProc::onto_plot2(cl, cl3k)
+#' }
+lk_ontoProc = function() NULL
